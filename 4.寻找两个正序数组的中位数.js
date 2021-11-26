0f3f966dd2b1fd22f5fcfeb5d14693e74a2c6096
苍旻 <55022033+SomiaWhiteRing@users.noreply.github.com>
2021-11-26 10:10:43 +0800

    做题做题

---
+++ 4.寻找两个正序数组的中位数.js
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=javascript
+ *
+ * [4] 寻找两个正序数组的中位数
+ *
+ * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/description/
+ *
+ * algorithms
+ * Hard (40.99%)
+ * Likes:    4698
+ * Dislikes: 0
+ * Total Accepted:    555.1K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,3]\n[2]'
+ *
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * 
+ * 算法的时间复杂度应该为 O(log (m+n)) 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums1 = [0,0], nums2 = [0,0]
+ * 输出：0.00000
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：nums1 = [], nums2 = [1]
+ * 输出：1.00000
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：nums1 = [2], nums2 = []
+ * 输出：2.00000
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -10^6 <= nums1[i], nums2[i] <= 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+
+const nums = nums1.concat(nums2)
+for(let i = 0 ;i < nums.length-1 ;i++){
+  let done = true
+  for(let j = 0;j < nums.length-1-i ;j++){
+    if(nums[j] > nums[j+1]){
+      const temp = nums[j+1]
+      nums[j+1] = nums[j]
+      nums[j] = temp
+      done = false
+    }
+  }
+  if(done){break}
+}
+if(nums.length%2 === 1){
+  return nums[(nums.length-1)/2]
+}else{
+  return (nums[nums.length/2] + nums[nums.length/2-1])/2
+}
+};
+// @lc code=end
+
---
+++ 5.最长回文子串.js
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=javascript
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (35.75%)
+ * Likes:    4380
+ * Dislikes: 0
+ * Total Accepted:    794.9K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '"babad"'
+ *
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s = "a"
+ * 输出："a"
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：s = "ac"
+ * 输出："a"
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * s 仅由数字和英文字母（大写和/或小写）组成
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  const max = s.length
+  let back = [0,0] // 返回的回文串两侧的指针
+  let maxlength = 0 // 记录目前保存的最长回文串长度-1
+  for(let i = 0; i < max-maxlength-1; i++){ // 检测的左指针
+    for(let j = i + 1; j < max; j++){ // 检测的右指针
+      if(s[i] === s[j] // 如果左右指针相等,检测其划定的字符串是否回文
+        && (j-i) > maxlength){ //将检测当前可能是的回文串是否最长挪至前部
+        let done = true // 制作回文判定
+        let step = Math.floor((j-i)/2) // n需要走的步数为(字符串的长度-1)/2
+        for(let n = 1; n <= step; n++){
+          if(s[i+n] !== s[j-n]){
+            done = false // 如果划定范围内不构成回文,退出检测循环
+            break
+          }
+        }
+        if(done){ //检测完毕仍构成回文
+            back = [i,j] //把当前找到的回文坐标填入记录
+            maxlength = j - i
+        }
+      }
+    }
+  }
+  return s.substring(back[0],back[1]+1)
+};
+// @lc code=end
+
